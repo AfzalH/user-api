edf07fc907400c782e507454e21f12cddac24853
Afzal Hossain <afzal.csedu@gmail.com>
2018-12-19 18:36:18 +0600

    assign permission

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -4,8 +4,6 @@ namespace AfzalH\UserApi\Controllers;
 
 
 use AfzalH\UserApi\Requests\StoreUser;
-use App\Http\Controllers\Controller;
-use App\User;
 
 class UserController extends BaseController
 {
--- tests/UserPermissionTest.php
+++ tests/UserPermissionTest.php
@@ -10,6 +10,7 @@ namespace AfzalH\UserApi\Tests;
 
 
 use Laravel\Passport\Passport;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class UserPermissionTest extends BaseTest
@@ -86,5 +87,86 @@ class UserPermissionTest extends BaseTest
 
         $user->refresh();
         $this->assertFalse($user->hasRole($role->id));
+    }
+
+    /** @test
+     * @throws \Exception
+     */
+    public function a_user_with_manage_users_permission_can_assign_a_permission_by_permission_name()
+    {
+        $admin = $this->getUserWithManageUsersPermission();
+        Passport::actingAs($admin);
+
+        $user = $this->createAUser();
+        $permission = Permission::create(['name' => 'create user']);
+        $permission->refresh();
+        $r = $this->post($this->prefix . 'users/assign-permission', [
+            'user_id' => $user->id,
+            'permission' => $permission->name
+        ]);
+        $r->assertStatus(202);
+
+        $user->refresh();
+        $this->assertTrue($user->hasPermissionTo($permission->name));
+    }
+
+    /** @test
+     * @throws \Exception
+     */
+    public function a_user_with_manage_users_permission_can_assign_a_permission_by_permission_id()
+    {
+        $admin = $this->getUserWithManageUsersPermission();
+        Passport::actingAs($admin);
+
+        $user = $this->createAUser();
+        $permission = Permission::create(['name' => 'create user']);
+        $permission->refresh();
+        $r = $this->post($this->prefix . 'users/assign-permission', [
+            'user_id' => $user->id,
+            'permission' => $permission->id
+        ]);
+        $r->assertStatus(202);
+
+        $user->refresh();
+        $this->assertTrue($user->hasPermissionTo($permission->name));
+    }
+
+    /** @test
+     * @throws \Exception
+     */
+    public function a_user_without_manage_users_permission_cannot_assign_a_permission_by_permission_name()
+    {
+        $actor = $this->createAUser();
+        Passport::actingAs($actor);
+
+        $user = $this->createAUser();
+        $permission = Permission::create(['name' => 'create user']);
+        $permission->refresh();
+        $r = $this->post($this->prefix . 'users/assign-permission', [
+            'user_id' => $user->id,
+            'permission' => $permission->id
+        ]);
+        $r->assertStatus(403);
+
+        $user->refresh();
+        $this->assertFalse($user->hasPermissionTo($permission->name));
+    }
+
+    /** @test
+     * @throws \Exception
+     */
+    public function a_guest_cannot_assign_a_permission_by_permission_name()
+    {
+        $user = $this->createAUser();
+        $permission = Permission::create(['name' => 'create user']);
+        $permission->refresh();
+        $r = $this->post($this->prefix . 'users/assign-permission', [
+            'user_id' => $user->id,
+            'permission' => $permission->id
+        ]);
+        $r->assertStatus(401);
+
+        $user->refresh();
+        $this->assertFalse($user->hasPermissionTo($permission->name));
     }
 }
